e5636863623ffc095e02cd2832c8d46813bbb3de
zviomestvi <geomestvi@gmail.com>
2021-12-08 21:47:17 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd4c98eb6-d22e-4df4-92d4-e4d7cd234c93';
+export const MODAL_CLOSE_SEC = 2.5;
